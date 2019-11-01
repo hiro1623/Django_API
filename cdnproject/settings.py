c603b321b64048ff8d6654ed5e47bd35f84e5eb1
hiro1623 <leo1623.g@gmail.com>
2019-11-02 07:10:10 +0900

    4th commit

--- cdnproject/settings.py
+++ cdnproject/settings.py
@@ -51,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'cdnproject.urls'
